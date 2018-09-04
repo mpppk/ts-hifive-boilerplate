395f95ff962a38f0a50e470df01e492bd732831f
mpppk <niboshiporipori@gmail.com>
2018-09-05 08:39:33 +0900

    Add production build

---
+++ webpack.prod.config.js
@@ -0,0 +1,26 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/app.ts',
+  output: {
+    filename: 'dist/bundle.js'
+  },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: ['.ts', '.tsx', '.js'] // note if using webpack 1 you'd also need a '' in the array as well
+  },
+  module: {
+    rules: [ // loaders will work with webpack 1 or 2; but will be renamed "rules" in future
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: 'ts-loader' },
+      { test: /\.ejs?$/, loader: 'ejs-compiled-loader' }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: '!!ejs-compiled-loader!src/assets/index.ejs'
+    })
+  ]
+};
