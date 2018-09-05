70fc139ce9c5fa7f79d4ace0107d367d5bcd7b10
mpppk <niboshiporipori@gmail.com>
2018-09-05 11:03:01 +0900

    Upgrade TypeScript to 3.0 and add strict compile checks

--- src/controller/RootController.ts
+++ src/controller/RootController.ts
@@ -1,28 +1,27 @@
-import * as JQuery from 'jquery';
-import {EventCallback, IContext} from '../h5/IContext';
-import {IController} from '../h5/IController';
-import countUpController, {ICountUpController} from './CountUpController';
-import resetController, {IResetController} from './ResetController';
+import { EventCallback, IContext } from '../h5/IContext';
+import { IController } from '../h5/IController';
+import countUpController, { ICountUpController } from './CountUpController';
+import resetController, { IResetController } from './ResetController';
 
 interface IRootController extends IController {
-    _countUpController: ICountUpController;
-    _resetController: IResetController;
-    '{rootElement} resetCount': EventCallback;
+  _countUpController: ICountUpController;
+  _resetController: IResetController;
+  '{rootElement} resetCount': EventCallback;
 }
 
 const rootController: IRootController = {
-    __meta: {
-        _countUpController: {
-            rootElement: '#counterContainer',
-        },
-    },
-    __name: 'rootController',
-    _countUpController: countUpController,
-    _resetController: resetController,
+  __meta: {
+    _countUpController: {
+      rootElement: '#counterContainer'
+    }
+  },
+  __name: 'rootController',
+  _countUpController: countUpController,
+  _resetController: resetController,
 
-    '{rootElement} resetCount'(context?: IContext, $el?: JQuery<HTMLElement>) {
-        this._countUpController.reset();
-    },
+  '{rootElement} resetCount'(_context?: IContext, _$el?: JQuery<HTMLElement>) {
+    this._countUpController.reset();
+  }
 };
 
 export default rootController;
